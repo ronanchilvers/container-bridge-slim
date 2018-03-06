4c9465f77eda88894fcecf115399217f4a6eb828
Ronan Chilvers <ronan@d3r.com>
2018-03-06 22:31:10 +0000

    Base setup for unit tests

---
+++ tests/ContainerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ronanchilvers\Container\Slim\Test;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Base test case for slim container
+ *
+ * @author Ronan Chilvers <ronan@d3r.com>
+ */
+class ContainerTest extends TestCase
+{
+}
